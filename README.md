5e95b842805c2d756e6fe4613254b17d984d9fa1
aubymori <aubyomori@gmail.com>
2022-09-17 13:11:41 -0500

    Initial commit 2: Electric Boogaloo

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# YouTubePatchCollection
-Allows for changing yt.config_ values
+# YouTube Patch Collection
+
+The latest and greatest experiment flag changer. Allows for changing normal flags **and** player flags on both Chrome and Firefox, consistently, with the same script for both.
+
+*(Note: Currently, changing player flags does not work on Firefox. It is unknown why it does not, and I am looking into it.)*
+
+## Preview
+
+![Watch page with flags changed](assets/Preview.png)
+
+Here's the script running on Vivaldi (Chromium based). As you can see, we've changed both normal config flags, and player experiment flags.
+
+## Installation
+
+Make sure you have a UserScript manager like Tampermonkey, then click [here](https://raw.githubusercontent.com/aubymori/YouTubePatchCollection/main/YouTubePatchCollection.user.js).
+
+## A bit of history...
+
+This started with the "YouTube Config Editor" script which was... pretty awful to say the least. Literally all it did was do a `setInterval` every 1ms which set the flags and then cleared it after 1 second. I could not get it to load on Chrome a single time. The code is actually so tiny that I can put the entire thing here.
+
+```js
+(function() {
+    window['yt'] = window['yt'] || {};
+    yt['config_'] = yt.config_ || {};
+    yt.config_['EXPERIMENT_FLAGS'] = yt.config_.EXPERIMENT_FLAGS || {};
+
+    var iv = setInterval(function() {
+        yt.config_.EXPERIMENT_FLAGS.kevlar_system_icons = false;
+    }, 1);
+
+    var to = setTimeout(function() {
+        clearInterval(iv);
+    }, 1000)
+})();
+```
+
+Yikes. And then there was an actually good script! It had an actual method of setting the flags instead of just setting them every 1ms. It would set them very consistently, and it could change player flags too! Oh, and also, it only worked on Firefox. It uses the `beforescriptexecute` event, which is nonstandard and only works on Firefox. So any Chrome users were out of the equation.
---
+++ YouTubePatchCollection.user.js
@@ -0,0 +1,166 @@
+// ==// ==UserScript==
+// @name         YouTube Patch Collection
+// @version      2.0.0
+// @description  Allows for changing of yt.config_ values
+// @author       Aubrey Pankow (aubyomori@gmail.com)
+// @author       Taniko Yamamoto (kirasicecreamm@gmail.com)
+// @license      Unlicense
+// @updateURL    https://raw.githubusercontent.com/aubymori/YouTubePatchCollection/main/YouTubePatchCollection.user.js
+// @match        www.youtube.com/*
+// @icon         https://www.youtube.com/favicon.ico
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+// Attributes to remove from <html>
+const ATTRS = [
+    "system-icons",
+    "typography",
+    "typography-spacing"
+];
+
+// Regular config keys.
+const CONFIGS = {
+    BUTTON_REWORK: true
+}
+
+// Experiment flags.
+const EXPFLAGS = {
+    kevlar_system_icons: false,
+    kevlar_watch_metadata_refresh: false
+}
+
+// Player flags
+// !!! USE STRINGS FOR VALUES !!!
+// For example: "true" instead of true
+const PLYRFLAGS = {
+    web_player_move_autonav_toggle: "false"
+}
+
+class YTP {
+    static observer = new MutationObserver(this.onNewScript);
+
+    static _config = {};
+
+    static isObject(item) {
+        return (item && typeof item === "object" && !Array.isArray(item));
+    }
+    
+    static mergeDeep(target, ...sources) {
+        if (!sources.length) return target;
+        const source = sources.shift();
+    
+        if (this.isObject(target) && this.isObject(source)) {
+            for (const key in source) {
+                if (this.isObject(source[key])) {
+                    if (!target[key]) Object.assign(target, { [key]: {} });
+                    this.mergeDeep(target[key], source[key]);
+                } else {
+                    Object.assign(target, { [key]: source[key] });
+                }
+            }
+        }
+    
+        return this.mergeDeep(target, ...sources);
+    }
+    
+
+    static onNewScript(mutations) {
+        for (var mut of mutations) {
+            for (var node of mut.addedNodes) {
+                YTP.bruteforce();
+            }
+        }
+    }
+
+    static start() {
+        this.observer.observe(document, {childList: true, subtree: true});
+    }
+    
+    static stop() {
+        this.observer.disconnect();
+    }
+
+    static bruteforce() {
+        if (!window.yt) return;
+        if (!window.yt.config_) return;
+
+        this.mergeDeep(window.yt.config_, this._config);
+    }
+
+    static setCfg(name, value) {
+        this._config[name] = value;
+    }
+
+    static setCfgMulti(configs) {
+        this.mergeDeep(this._config, configs);
+    }
+
+    static setExp(name, value) {
+        if (!("EXPERIMENT_FLAGS" in this._config)) this._config.EXPERIMENT_FLAGS = {};
+
+        this._config.EXPERIMENT_FLAGS[name] = value;
+    }
+
+    static setExpMulti(exps) {
+        if (!("EXPERIMENT_FLAGS" in this._config)) this._config.EXPERIMENT_FLAGS = {};
+
+        this.mergeDeep(this._config.EXPERIMENT_FLAGS, exps);
+    }
+
+    static decodePlyrFlags(flags) {
+        var obj = {},
+            dflags = flags.split("&");
+
+        for (var i = 0; i < dflags.length; i++) {
+            var dflag = dflags[i].split("=");
+            obj[dflag[0]] = dflag[1];
+        }
+        
+        return obj;
+    }
+
+    static encodePlyrFlags(flags) {
+        var keys = Object.keys(flags),
+            response = "";
+
+        for (var i = 0; i < keys.length; i++) {
+            if (i > 0) {
+                response += "&";
+            }
+            response += keys[i] + "=" + flags[keys[i]];
+        }
+
+        return response;
+    }
+
+    static setPlyrFlags(flags) {
+        if (!window.yt) return;
+        if (!window.yt.config_) return;
+        if (!window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS) return;
+        var conCfgs = window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS;
+        if (!("WEB_PLAYER_CONTEXT_CONFIGS" in this._config)) this._config.WEB_PLAYER_CONTEXT_CONFIGS = {};
+
+        for (var cfg in conCfgs) {
+            var dflags = this.decodePlyrFlags(conCfgs[cfg].serializedExperimentFlags);
+            this.mergeDeep(dflags, flags);
+            this._config.WEB_PLAYER_CONTEXT_CONFIGS[cfg] = {
+                serializedExperimentFlags: this.encodePlyrFlags(dflags)
+            }
+        }
+    }
+}
+
+window.addEventListener("yt-page-data-updated", function tmp() {
+    YTP.stop();
+    for (i = 0; i < ATTRS.length; i++) {
+        document.getElementsByTagName("html")[0].removeAttribute(ATTRS[i]);
+    }
+    window.removeEventListener("yt-page-date-updated", tmp);  
+});
+
+YTP.start();
+
+YTP.setCfgMulti(CONFIGS);
+YTP.setExpMulti(EXPFLAGS);
+YTP.setPlyrFlags(PLYRFLAGS);
